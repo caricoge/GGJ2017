2a68028aa1bf68628c3cb98148ad3f6bf96e5911
Hugo <hugoguillou@hotmail.fr>
2017-01-21 16:28:46 +0100

    open Door

---
+++ GGJ2017/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	bool open = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open) {
+			transform.Translate (Vector3.down * Time.deltaTime * 1.5f);
+		}
+	}
+
+	void OnTriggerStay(Collider coll)
+	{
+		if (Input.GetKeyDown (KeyCode.E)) {
+			Debug.Log("Click");
+			open = true;
+		}
+	}
+}
