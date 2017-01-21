1938db1a52f696d06a7f1c8cdc7e154cf9fee824
Hugo <hugoguillou@hotmail.fr>
2017-01-21 12:23:28 +0100

    no message

---
+++ GGJ2017/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent nav_mesh;
+
+	// Use this for initialization
+	void Start () {
+
+		nav_mesh = GetComponent<NavMeshAgent> ();
+		nav_mesh.SetDestination (transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
