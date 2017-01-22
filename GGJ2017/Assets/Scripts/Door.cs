1efa71aa06408877cf3eee15e23909100c051941
Hugo <hugoguillou@hotmail.fr>
2017-01-22 01:15:22 +0100

    Last Mecanisms

--- GGJ2017/Assets/Scripts/Door.cs
+++ GGJ2017/Assets/Scripts/Door.cs
@@ -1,42 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour {
 
 	bool open = false;
 
+	Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		animator = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (open) {
-			transform.Translate (Vector3.down * Time.deltaTime * 1.5f);
-		}
+		
 	}
 
 	void OnTriggerStay(Collider coll)
 	{
-		
 		if (Input.GetKeyDown (KeyCode.E))
 		{
-			Debug.Log ("Click");
+			Debug.Log (coll.gameObject.tag);
 			if (coll.gameObject.tag == "Player")
 			{
-				Debug.Log ("Player");
+				Debug.Log ("Click");
 				Player player = coll.GetComponent<Player> ();
 
-				if (player.HasKey () && !player.IsAfraid()) {
-					open = true;
+				if (player.HasKey () && !player.IsAfraid()) 
+				{
+					animator.SetBool ("Activate", true);
+					Debug.Log ("Player");
+					SceneManager.LoadScene ("Scenes/WinScene");
 				}
+
 			}
-
 		}
-
-
 	}
+
 }
---
+++ GGJ2017/Assets/Scripts/Restart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+	Animator animator;
+	int restartNameCode;
+	// Use this for initialization
+	void Start () {
+
+		animator = FindObjectOfType<Canvas> ().GetComponent<Animator>();
+		restartNameCode = Animator.StringToHash ("Base.Restart");
+		Debug.Log (animator.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Restart")) {
+			SceneManager.LoadScene ("Scenes/TestCharacter");
+
+		}
+	}
+}
